5d265c92f2048773b3644f74af7193b797558b8d
SethMC26 <holtzmans@merrimack.edu>
2024-04-27 02:21:58 -0400

    Feat: Implemented Messages and Jondo as JSON representations

---
+++ src/Model/Jondo.java
@@ -0,0 +1,139 @@
+/*
+ * free (adj.): unencumbered; not under the control of others
+ * Written by Seth Holtzman in 2024 and released into the public domain
+ * with no warranty of any kind, either expressed or implied.
+ * It probably won't make your computer catch on fire, or eat
+ * your children, but it might.  Use at your own risk.
+ *
+ * Oh but what's a constant among friends?
+ */
+
+package Model;
+
+import merrimackutil.json.JSONSerializable;
+import merrimackutil.json.types.JSONObject;
+import merrimackutil.json.types.JSONType;
+
+import java.io.InvalidObjectException;
+import java.nio.ByteBuffer;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+/**
+ * Internal representation of a Model.Jondo
+ */
+public class Jondo implements JSONSerializable {
+    private String addr;
+    private int port;
+    private String uid;
+
+    /**
+     * Creates a new Model.Jondo based on address and port
+     *
+     * @param _addr String IP address of Model.Jondo
+     * @param _port Int Port of this Model.Jondo
+     */
+    public Jondo(String _addr, int _port) {
+        addr = _addr;
+        port = _port;
+
+        //Create UID from addr and port
+        createUID();
+    }
+
+    public Jondo(JSONObject jondoJSON) throws InvalidObjectException {
+        deserialize(jondoJSON);
+    }
+
+    @Override
+    public String serialize() {
+        return toJSONType().toJSON();
+    }
+
+    @Override
+    public void deserialize(JSONType jsonType) throws InvalidObjectException {
+        if (!(jsonType instanceof JSONObject))
+            throw new InvalidObjectException("JSONObject expected.");
+
+        JSONObject jondoJSON = (JSONObject) jsonType;
+
+        if (!(jondoJSON.containsKey("addr") || jondoJSON.containsKey("port"))) {
+            throw new InvalidObjectException("JSONObject does not have TYPE field");
+        }
+
+        addr = jondoJSON.getString("addr");
+        port = jondoJSON.getInt("port");
+
+        //create UID based on addr and port
+        createUID();
+    }
+
+    /**
+     * Converts Model.Jondo(Node) to JSON object
+     *
+     * @return JSONObject which is the Model.Jondo
+     */
+    @Override
+    public JSONType toJSONType() {
+        JSONObject jondoJSON = new JSONObject();
+
+        jondoJSON.put("addr",addr);
+        jondoJSON.put("port",port);
+
+        return jondoJSON;
+    }
+
+    /**
+     * Gets IP address of Model.Jondo
+     * @return String of IP address
+     */
+    public String getAddr() {
+        return addr;
+    }
+
+    /**
+     * Gets Port of Model.Jondo
+     * @return int port
+     */
+    public int getPort() {
+        return port;
+    }
+
+    /**
+     * Gets UID of this node which is 32 bit(4 byte) hash based on addr and port
+     * @return String 32 bit(4 byte) hash
+     */
+    public String getUid() {
+        return uid;
+    }
+
+    @Override
+    public String toString() {
+        return toJSONType().getFormattedJSON();
+    }
+
+    /**
+     * Compute the nodes ID by taking the SHA-1 hash of the
+     * IP address and port pair to determine the UID of this
+     * node.
+     *
+     * From Proj 4 Written by Zachary Kissel
+     */
+    private void createUID()
+    {
+        ByteBuffer buff = ByteBuffer.allocate(4);
+        buff.putInt(this.port);
+        try
+        {
+            MessageDigest hash = MessageDigest.getInstance("SHA-1");
+            hash.update(this.addr.getBytes());
+            hash.update(buff.array());
+            this.uid = Base64.getEncoder().encodeToString(hash.digest());
+        }
+        catch (NoSuchAlgorithmException e)
+        {
+            System.err.println("Internal Error: SHA1 hash not supported.");
+        }
+    }
+}
---
+++ src/Model/Message.java
@@ -0,0 +1,304 @@
+/*
+ * free (adj.): unencumbered; not under the control of others
+ * Written by Seth Holtzman in 2024 and released into the public domain
+ * with no warranty of any kind, either expressed or implied.
+ * It probably won't make your computer catch on fire, or eat
+ * your children, but it might.  Use at your own risk.
+ *
+ * Oh but what's a constant among friends?
+ */
+package Model;
+
+import merrimackutil.json.JSONSerializable;
+import merrimackutil.json.types.JSONObject;
+import merrimackutil.json.types.JSONType;
+import static merrimackutil.json.JsonIO.readObject;
+
+import java.io.InvalidObjectException;
+
+/**
+ * JSON messages sent between nodes
+ */
+public class Message implements JSONSerializable {
+    /**
+     * Type of message we are sending
+     *  Hello, Welcome, Data, Broadcast
+     */
+    private String type;
+    /**
+     * Source Address of message
+     */
+    private String srcAddr;
+    /**
+     * Source Port of message
+     */
+    private int srcPort;
+    /**
+     * Routing Table object
+     */
+    private Object routingTable;
+    /**
+     * Destination IP address
+     */
+    private String dstAddr;
+    /**
+     * Destination Port
+     */
+    private int dstPort;
+    /**
+     * Destination Data
+     */
+    private String data; //might change later to a different type
+
+    /**
+     * New node to add to routing table
+     */
+    private Jondo newNode;
+
+    /**
+     * Creates Model.Message Object from Builder
+     *
+     * @param builder builder to create
+     */
+    private Message(Builder builder) {
+        type = builder.type;
+        srcAddr = builder.srcAddr;
+        srcPort = builder.srcPort;
+        routingTable = builder.routingTable;
+        newNode = builder.newNode;
+        dstAddr = builder.dstAddr;
+        dstPort = builder.dstPort;
+        data = builder.data;
+    }
+
+    /**
+     * Deserializes Message from JSON Object
+     *
+     * @param messageJSON JSONObject to deserialize
+     * @throws InvalidObjectException Throws if JSON is invalid such as invalid fields
+     */
+    public Message(JSONObject messageJSON) throws InvalidObjectException {
+        deserialize(messageJSON);
+    }
+
+    /**
+     * Serializes the JSON object into JSON string representation
+     *
+     * @return JSON String
+     */
+    @Override
+    public String serialize() {
+        return toJSONType().toJSON();
+    }
+
+    /**
+     * Deserialize the JSONObject into internal message representation
+     *
+     * @param jsonType JSONObject that we want to deserialize
+     * @throws InvalidObjectException Throws if JSON does not contain expected keys or if it does not have JSONObject
+     */
+    @Override
+    public void deserialize(JSONType jsonType) throws InvalidObjectException {
+        if (!(jsonType instanceof JSONObject))
+            throw new InvalidObjectException("JSONObject expected.");
+
+        JSONObject messageJSON = (JSONObject) jsonType;
+
+        if (!messageJSON.containsKey("type")) {
+            throw new InvalidObjectException("Model.Message json does not have TYPE field");
+        }
+
+        //get Type of message
+        type = messageJSON.getString("type");
+
+        //deserialize JSONObject based on type of JSON
+        switch(type) {
+            case "HELLO":
+                if (!(messageJSON.containsKey("srcAddr") && messageJSON.containsKey("srcPort"))) {
+                    throw new InvalidObjectException("HELLO Model.Message should contain srcAddr and srcPort");
+                }
+
+                srcAddr = messageJSON.getString("srcAddr");
+                srcPort = messageJSON.getInt("srcPort");
+                break;
+            case "WELCOME":
+                if (!messageJSON.containsKey("routingTable")) {
+                    throw new InvalidObjectException("WELCOME message should contain routingTable");
+                }
+
+                routingTable = messageJSON.getObject("routingTable");
+                break;
+            case "BROADCAST":
+                if (!messageJSON.containsKey("newNode")) {
+                    throw new InvalidObjectException("BROADCAST message should contain new Node");
+                }
+
+                JSONObject jondoJSON = readObject(messageJSON.getString("newNode"));
+
+                newNode = new Jondo(jondoJSON);
+                break;
+            case "DATA":
+                if (!(messageJSON.containsKey("dstAddr") || messageJSON.containsKey("dstPort") ||
+                        messageJSON.containsKey("data"))) {
+                    throw new InvalidObjectException("DATA message should contain dstAddr, dstPort, data");
+                }
+
+                dstAddr = messageJSON.getString("dstAddr");
+                dstPort = messageJSON.getInt("dstPort");
+                data = messageJSON.getString("data");
+                break;
+            default:
+                throw new IllegalArgumentException("Bad type - Must be HELLO, WELCOME, BROADCAST, DATA");
+        }
+    }
+
+    /**
+     * Creates JSONObject based on type of message
+     *
+     * @return JSONObject of message
+     */
+    @Override
+    public JSONType toJSONType() {
+        JSONObject messageJSON = new JSONObject();
+
+        //switch between types to and put needed fields into JSONObject
+        switch (type) {
+            case "HELLO":
+                messageJSON.put("type",type);
+                messageJSON.put("srcAddr",srcAddr);
+                messageJSON.put("srcPort",srcPort);
+
+                return messageJSON;
+            case "WELCOME":
+                messageJSON.put("type",type);
+                messageJSON.put("routingTable",routingTable);
+
+                return messageJSON;
+            case "BROADCAST":
+                messageJSON.put("type",type);
+                messageJSON.put("newNode",newNode.serialize());
+
+                return messageJSON;
+            case "DATA":
+                messageJSON.put("type",type);
+                messageJSON.put("dstAddr",dstAddr);
+                messageJSON.put("dstPort",dstPort);
+                messageJSON.put("data",data);
+
+                return messageJSON;
+            default:
+                throw new IllegalArgumentException("Bad type - Must be HELLO, WELCOME, BROADCAST, DATA");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return toJSONType().getFormattedJSON();
+    }
+
+    /**
+     * Gets type of message
+     * @return String of type
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * Gets Source IP Address
+     * @return String of IP Address
+     */
+    public String getSrcAddr() {
+        return srcAddr;
+    }
+
+    /**
+     * Gets Source Port
+     * @return int of port
+     */
+    public int getSrcPort() {
+        return srcPort;
+    }
+
+    /**
+     * Builder class to make Model.Message Object
+     */
+    public static class Builder {
+        private String type;
+        private String srcAddr;
+        private int srcPort;
+        private Object routingTable;
+        private String dstAddr;
+        private int dstPort;
+        private String data; //might change later to a different type
+        private Jondo newNode;
+
+        /**
+         * Creates basic message object
+         * @param _type String type field of message: HELLO, WELCOME, BROADCAST, DATA
+         */
+        public Builder(String _type) {
+            type = _type;
+        }
+
+        /**
+         * Creates HELLO message object
+         * @param _srcAddr Source IP address of Model.Jondo trying to join network
+         * @param _srcPort Port Int address of Model.Jondo trying to join network
+         *
+         * @return this Builder
+         */
+        public Builder setHello(String _srcAddr, int _srcPort) {
+            srcAddr = _srcAddr;
+            srcPort = _srcPort;
+            return this;
+        }
+
+        /**
+         * Creates WELCOME message
+         * @param _routingTable Routing Table to send to new node
+         *
+         * @return this Builder
+         */
+        public Builder setWelcome(Object _routingTable) {
+            routingTable = _routingTable;
+            return this;
+        }
+
+        /**
+         * Creates BROADCAST message
+         *
+         * @param _newNode Model.Jondo to add to routingTable
+         * @return this Builder
+         */
+        public Builder setBroadcast(Jondo _newNode) {
+            newNode = _newNode;
+            return this;
+        }
+
+        /**
+         * Creates DATA message
+         *
+         * @param _dstAddr IP address of destination of message
+         * @param _dstPort Port of destination of Model.Message
+         * @param _data Data to send to destination
+         *
+         * @return this Builder
+         */
+        public Builder setData(String _dstAddr, int _dstPort, String _data) {
+            dstAddr = _dstAddr;
+            dstPort = _dstPort;
+            data = _data;
+            return this;
+        }
+
+        /**
+         * Builds Model.Message
+         *
+         * @return Model.Message from builder
+         */
+        public Message build() {
+            return new Message(this);
+        }
+    }
+}
