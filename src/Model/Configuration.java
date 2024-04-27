d947af3e52a1ca650c2bfa5969a6a6faba5be5bf
SethMC26 <holtzmans@merrimack.edu>
2024-04-27 02:20:58 -0400

    Feat: Addded preliminary Configuration file to add config builds later

---
+++ src/Model/Configuration.java
@@ -0,0 +1,13 @@
+/*
+ * free (adj.): unencumbered; not under the control of others
+ * Written by Seth Holtzman in 2024 and released into the public domain
+ * with no warranty of any kind, either expressed or implied.
+ * It probably won't make your computer catch on fire, or eat
+ * your children, but it might.  Use at your own risk.
+ *
+ * Oh but what's a constant among friends?
+ */
+package Model;
+
+public class Configuration {
+}
