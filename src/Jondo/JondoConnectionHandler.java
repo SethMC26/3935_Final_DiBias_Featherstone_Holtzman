092e292eb7e183a49e362415fa3837dd3050b875
Seth H <holtzmans@merrimack.edu>
2024-04-28 00:24:43 -0400

    Feat: Implemented support to handle DATA message and create virtual tunnel

--- src/Jondo/JondoConnectionHandler.java
+++ src/Jondo/JondoConnectionHandler.java
@@ -14,27 +14,54 @@ import Model.Node;
 import merrimackutil.json.types.JSONObject;
 
 import java.io.IOException;
-import java.io.InvalidObjectException;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Random;
 import java.util.Scanner;
 import java.util.concurrent.ConcurrentHashMap;
 
 import static merrimackutil.json.JsonIO.readObject;
 
+/**
+ * Handle the incoming connections for each Jondo
+ */
 public class JondoConnectionHandler implements Runnable{
+    /**
+     * Socket of incoming connection
+     */
     private Socket sock;
-    private Jondo jondo;
+    /**
+     * Routingtable
+     */
     private ConcurrentHashMap<String, Node> routingTable;
+    /**
+     * A number between 1-100 which represent the percentage chance of heads * 100
+     */
+    private int probHead;
+    /**
+     * Random number generator
+     */
+    private Random randGen;
 
-    public JondoConnectionHandler(Socket _sock, Jondo _jondo) {
+    /**
+     * Creates new Connection hander
+     * @param _sock Socket we received connection on
+     * @param _routingTable Routing Table ConcurrentHashMap with keys being string UID and values being Node object
+     */
+    public JondoConnectionHandler(Socket _sock, ConcurrentHashMap<String, Node> _routingTable) {
         sock = _sock;
-        jondo = _jondo;
 
         //get routing table from JONDO
-        routingTable = jondo.getRoutingTable();
+        routingTable = _routingTable;
+
+        // 2/3 chance of fording node
+        probHead = 66;
+        randGen = new Random();
     }
 
+    /**
+     * Run method handles connection new thread
+     */
     @Override
     public void run() {
         try {
@@ -49,23 +76,85 @@ public class JondoConnectionHandler implements Runnable{
             Message recvMessage = new Message(recvJSON);
 
             switch (recvMessage.getType()) {
+                //we get broadcast from Blender of a new node joining network
                 case "BROADCAST":
                     //get new Jondo to add from message and add it to routing table
                     Node newJondo = new Node(recvMessage.getSrcAddr(),recvMessage.getSrcPort());
-                    jondo.addJondo(newJondo);
+
+                    //check if node is already in our routing table
+                    if (routingTable.containsKey(newJondo.getUid())) {
+                        System.err.println("Blender: Error adding Jondo, already in Routing Table");
+                        return;
+                    }
+
+                    //put node in routing table
+                    routingTable.put(newJondo.getUid(), newJondo);
                     break;
+                //we are forwarded data from another node
                 case "DATA":
-                    throw new IllegalArgumentException("Data case must be handled");
+                    Socket nodeSock = null;
+                    Scanner nodeRecv = null;
+                    PrintWriter nodeSend = null;
+
+                    //flip a coin if heads(true) forward message if tails(false) send it to destination
+                    //Coin flip merely determines who we send messages to(socket we connect to)
+                    if (flipCoin()) {
+                        //send message to random node
+                        //get a list of our keys(Node UIDS)
+                        Object[] nodeList = routingTable.keySet().toArray();
+                        //get a random number to pick a random index from keys
+                        int randNum = randGen.nextInt(nodeList.length);
+
+                        //get a random node
+                        Node randNode = routingTable.get((String) nodeList[randNum]);
+
+                        //connect to random node
+                        nodeSock = new Socket(randNode.getAddr(),randNode.getPort());
+                        //get streams
+                        nodeRecv = new Scanner(nodeSock.getInputStream());
+                        nodeSend = new PrintWriter(nodeSock.getOutputStream());
+
+                    }
+                    else {
+                        //coin flip was tails so we send node to destination
+                        //connect to destination and get input streams
+                        nodeSock = new Socket(recvMessage.getDstAddr(),recvMessage.getDstPort());
+                        nodeRecv = new Scanner(nodeSock.getInputStream());
+                        nodeSend = new PrintWriter(nodeSock.getOutputStream());
+                    }
+
+                    //forward original message to node/dst and wait for reply
+                    nodeSend.println(recvMessage.serialize());
+
+                    //get Reply string from node/dst
+                    String nodeReply = nodeRecv.nextLine();
+
+                    //send reply to original connected node
+                    send.println(nodeReply);
+
+                    //close connections
+                    nodeSock.close();
+                    sock.close();
+                    break;
                 default:
                     System.err.println("Jondo Connection: Message must be of DATA or Broadcast type");
                     System.err.println(recvMessage);
                     break;
             }
-
-        } catch (InvalidObjectException e) {
-            throw new RuntimeException(e);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    /**
+     * Flips a biased coin the bias being probHead
+     * We generate a random number between 0-100 if it is less than probHeads we return true
+     * @return
+     */
+    private boolean flipCoin() {
+        Random rand = new Random();
+        int ranNum = rand.nextInt(100);
+
+        return ranNum <= probHead;
     }
 }
