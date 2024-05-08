5ec9a9e245ed66d90fe1cfa202ca76665b8590b4
Seth H <holtzmans@merrimack.edu>
2024-05-08 00:02:51 -0400

    Chore: Removed old unused testing files

--- src/test/jondo2.java
+++
@@ -1,9 +0,0 @@
-package test;
-
-import Jondo.Jondo;
-
-public class jondo2 {
-    public static void main(String[] args) {
-       // Jondo jondo2 = new Jondo("127.0.0.1",6002,4,"127.0.0.1",4999);
-    }
-}
